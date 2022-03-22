a28d23d13f3d4cf98545f53aeac4a5ff0c4edb55
saurabh <srbh24@gmail.com>
2022-03-23 02:15:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import  Home from './pages/Home';
+
+
+const App = () => {
+  return <div > <Home /> < /div>;
+};
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems=[
+  {
+    id:1,
+    img:"https://i.ibb.co/W2Cvxmk/doc.png",
+    title:"HAPPY HEALTH, HAPPY LIFE!",
+    desc:"MEGA CASHBACK UPTO 30% ON FIRST ORDER",
+    bg:"f5fafd"
+  },
+  {
+    id:2,
+    img:"https://i.ibb.co/W2Cvxmk/doc.png",
+    title:"HAPPY LIFE, HAPPY LIFE!",
+    desc:"MEGA CASHBACK UPTO 30% ON FIRST ORDER",
+    bg:"f5fafd"
+  },
+  {
+    id:3,
+    img:"https://i.ibb.co/W2Cvxmk/doc.png",
+    title:"HAPPY WIFE, HAPPY LIFE!",
+    desc:"MEGA CASHBACK UPTO 30% ON FIRST ORDER",
+    bg:"fbf0f4"
+  },
+];
+
+export const categories = [
+  {
+    id:1,
+    img:"https://i.ibb.co/yybxSdQ/pexels-edward-jenner-4031821.jpg",
+    title:"DOCTORS"
+  },
+  {
+    id:2,
+    img:"https://i.ibb.co/Y8W9VvR/pexels-anna-tarazevich-5910949.jpg",
+    title:"PHARMACY"
+  },
+  {
+    id:3,
+    img:"https://i.ibb.co/w0mRMmP/pexels-rfstudio-3825435.jpg",
+    title:"LAB TESTS"
+  },
+];
+
+export const popularProducts = [
+  {
+    id:1,
+    img:"https://i.ibb.co/HDGYKbB/dettol-instant-hand-sanitizer-200-ml-500x500.jpg"
+  },
+  {
+    id:2,
+    img:"https://i.ibb.co/fpWBmd7/Body-Non-Contact-Infrared-Digital-Thermometer-Temperature-Gun.jpg"
+  },
+  {
+    id:3,
+    img:"https://i.ibb.co/WtRB5xz/volini-spray-40gm-0-1.png"
+  },
+  {
+    id:4,
+    img:"https://img.medicalexpo.com/images_me/photo-mg/95737-16456919.jpg"
+  },
+  {
+    id:5,
+    img:"https://newassets.apollo247.com/pub/media/catalog/product/v/i/vic0020_1.jpg"
+  },
+  {
+    id:6,
+    img:"https://m.media-amazon.com/images/I/611PHgj6q4L._SY450_.jpg"
+  },
+  {
+    id:7,
+    img:"https://m.media-amazon.com/images/I/618YbElLxYS._SY450_.jpg"
+  },
+  {
+    id:8,
+    img:"https://newassets.apollo247.com/pub/media/catalog/product/l/i/lis0010_1.jpg"
+  }
+]
